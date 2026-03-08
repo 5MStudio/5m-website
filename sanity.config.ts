6c7b0e1aba483ffe27bf4e17008d03ad8f1cf6e6
eonsoftime <moritz@buesselberg.net>
2026-03-09 00:03:43 +0100

    Build ready for Vercel deploy

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,18 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
-  name: 'default',
+  name: 'pdefault',
   title: '5M Website',
 
-  projectId: 'p8hrggg4',
+  projectId: 'p8hrggg4', // <-- replace with your actual projectId
   dataset: 'production',
+
+  // Add this line to set your Sanity Studio subdomain
+  hostname: '5m-studio', // <-- this will make your URL https://5m-studio.sanity.studio
 
   plugins: [structureTool(), visionTool()],
 
